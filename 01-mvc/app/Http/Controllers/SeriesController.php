d8e149bc3142f74bd5a540ad92898fc0932312c9
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2023-07-25 19:20:12 -0300

    Criando um layout com um componente blade

--- 01-mvc/app/Http/Controllers/SeriesController.php
+++ 01-mvc/app/Http/Controllers/SeriesController.php
@@ -14,6 +14,6 @@ class SeriesController extends Controller
             'Daredevil'
         ];
 
-        return view('listar-series', compact('series'));
+        return view('series.index')->with('series', $series);
     }
 }
--- 01-mvc/resources/views/components/layout.blade.php
+++ 01-mvc/resources/views/components/layout.blade.php
@@ -4,17 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso Alura</title>
+    <title>{{ $title }}</title>
 </head>
 
 <body>
 
-    <h1>Séries</h1>
-    <ul>
-        @foreach($series as $serie)
-        <li>{{ $serie }}</li>
-        @endforeach
-    </ul>
+    <h1>{{ $title }}</h1>
+
+    {{ $slot }}
 
 </body>
 
